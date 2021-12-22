92ac4d198bad2fd8c6c9862fb7dcab0a39497874
s-nashiro <nvmtest07@gmail.com>
2021-12-22 22:15:21 +0900

    RuboCop を導入

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 rails.application.config.generators do |g|
-    g.skip_routes true
-    g.assets false
-    g.helper false
+  g.skip_routes true
+  g.assets false
+  g.helper false
 end
