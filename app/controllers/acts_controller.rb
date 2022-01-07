039390649666dff8178da97f6b4c9157d8923fca
s-nashiro <nvmtest07@gmail.com>
2022-01-07 22:56:28 +0900

    テストデータ追加

--- app/controllers/acts_controller.rb
+++ app/controllers/acts_controller.rb
@@ -13,4 +13,29 @@ class ActsController < ApplicationController
   def create
     Act.create!(content: act_params[:content], user_id: current_user.id)
   end
+
+  def show
+    @act = Act.find(params[:id])
+  end
+
+
+  private
+
+  def act_params
+    params.require(:act).permit(:content)
+  end
+
+
+  def update
+    @act.update!(act_params)
+    redirect_to @act
+  end
+
+  # ***** 以下を追加 *****
+  def set_act
+    # 「自分の投稿」の中から URL の :id に対応する投稿を探す
+    # 「他人の投稿」の場合はエラーを出す
+    @act = current_user.acts.find_by(id: params[:id])
+    redirect_to root_path, alert: "権限がありません" if @act.nil?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :acts
+  has_many :acts, dependent: :destroy #dependent: :destroはユーザーを削除した際に，そのユーザーの投稿したメッセージも自動で削除されるようになる。
 end
